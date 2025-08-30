6d4ee6198e12fb8e9979c082b85feebbdc7d6a76
Edward <63866043+Telianedward@users.noreply.github.com>
2025-08-31 02:42:12 +0300

    Delete src directory

--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-// src/types/index.ts (или в colorPalettePresets.ts)
-// src/types/index.ts
-
-// export type PaletteName = 'default' | 'green' | 'pastel' | 'contrast' | 'github' | 'light';
-export type PaletteName = 'default';
